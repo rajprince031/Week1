5b15706f297001046c81aa10050a041cdba412d1
PRINCE RAJ <rajprince031@gmail.com>
2025-01-09 16:03:44 +0530

    HarryAgeCalculator

---
+++ Level 1 Practice Programs/HarryAgeCalculator.java
@@ -0,0 +1,20 @@
+// Create HarryAgeCalculator Class to calculate the age of harry
+
+public class HarryAgeCalculator{
+
+	public static void main(String[] agrs) {
+
+		// Create the variable age to indicate the birth year of harry
+		int age = 2000;
+			
+        // Create the variable presentYear to indicate the current year
+        int presentYear = 2024;
+
+		// Create the variable ageCalculation to indicate the current age of ha 
+        int ageCalculation = presentYear - age;
+
+        // Print the ageCalculation
+        System.out.println("Harry's age in 2024 is "+ageCalculation);
+	}
+
+}

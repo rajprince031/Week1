92d9811a48ecbb9696ef2b8af5d6f1dad44e3926
PRINCE RAJ <rajprince031@gmail.com>
2025-01-09 16:01:43 +0530

    ConvertKilomerterToMiles

---
+++ Level 1 Practice Programs/ConvertKilomerterToMiles.java
@@ -0,0 +1,20 @@
+// Create ConvertKilomerterToMiles class to convert the kilometers
+// to miles
+
+public class ConvertKilomerterToMiles {
+    public static void main(String[] agrs){
+    
+    // Create a variable kilometers to indicate the kilometers.
+    float kilometers = 10.8f;
+
+    // Create the variable calculateMiles that help to calculate the 
+    // miles using the formula (1 kilometer = 1.6 miles).
+    
+    float calculateMiles = kilometers * 1.6f;
+
+    //Print the calculateMiles
+    System.out.println(" The distance "+kilometers+" km in miles is "+calculateMiles);
+
+
+    }
+}
